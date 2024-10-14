7e4fbfeed75ff8338179875be56a9c8547a93649
Andrew Jowe <andrewjowe@Andrews-MBP.attlocal.net>
2024-10-14 16:28:47 -0700

    double check styling is consistent with Figma

--- components/section2/WhyJoin.tsx
+++ components/section2/WhyJoin.tsx
@@ -7,7 +7,7 @@ export default function WhyJoin() {
     <Spacer rem={1.5} />
     <p className="p1 text-dark-blue font-bold">Because we turn your skills into tangible results!</p>
     <Spacer rem={1.25} />
-    <p className="p2 leading-[1.6]">
+    <p className="p2 leading-[1.6] font-medium">
       Do you want to take your development/design skills to the next level? Are you ready to collaborate with a startup and create a product that makes an impact? The UC Davis Google Developer Student Club’s Product Cohort is the opportunity you've been waiting for.
       <br/><br/>
       In this hands-on cohort, you won’t just learn—you’ll do. You will join a small, dynamic team tasked with building a functional website for an innovative startup. Together, you’ll experience the thrill and challenges of working in  a fast-paced, real-world environment.
